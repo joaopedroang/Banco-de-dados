95110d3c4a72271a15bd309d40b7ec6676e47c3f
João Pedro <127750232+joaopedroang@users.noreply.github.com>
2023-04-27 10:15:52 -0300

    Delete Select#2.sql

--- Select#2.sql
+++
@@ -1,8 +0,0 @@
-SELECT nome, matricula, nome_curso, nome_disciplina FROM aluno
-INNER JOIN aluno_curso ON aluno_curso.aluno = matricula
-INNER JOIN curso ON aluno_curso.curso = cod_curso
-INNER JOIN aluno_turma ON aluno_turma.aluno = matricula 
-INNER JOIN turma ON aluno_turma.turma = cod_turma  
-INNER JOIN disciplina ON turma.disciplina = cod_disciplina
-WHERE matricula = "202300001" 
-
